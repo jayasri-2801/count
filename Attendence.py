92640def861dec1c0b29b6d837a21c3c1570362d
jayasri-2801 <jayasribalasubramaniyan06@gmail.com>
2026-02-03 05:04:09 +0530

    Create Attendence.py

---
+++ Attendence.py
@@ -0,0 +1,7 @@
+def mark_attendance():
+    roll_no = input("Enter roll number: ")
+    status = input("Enter attendance (Present/Absent): ")
+    print(f"Attendance marked for Roll No {roll_no}: {status}")
+
+def view_attendance():
+    print("Attendance records displayed here.")
